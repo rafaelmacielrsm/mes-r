8be4b879dad220214b35474ee6f3054dca2a57a7
rafaelmacielrsm <rafaelmacielrsm@gmail.com>
2016-11-29 18:28:53 -0300

    Apply the materialize-sass on categories views
    - Creates a helper method to generates a floating button to link back to
      another page.
    - Change most of the categories views to use the materialize-sass style
      and adds a modal on index to confirm a deletion of a record.
    - Adds internationalization files for theses views.
    - Adds a exception rule for show in the categories resource routes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def link_back(target)
+    link_to target, class: 'btn-floating btn-large waves-effect waves-light',
+      id: 'return-btn' do
+        fa_icon 'arrow-left'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :batches
-  resources :categories
+  resources :categories, except: :show
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
